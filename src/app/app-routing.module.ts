d829dd4e9bf28271818928bb5115fd14ece35af7
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-06-26 20:03:32 +0300

    lazy-loaded routes for auth, user, and room(movies) modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  // Redirecționare către home
 
+  { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
+  { path:'user', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
+  { path: 'room', loadChildren: () => import('./features/room/room.module').then(m => m.RoomModule) },
 
 ];
 
