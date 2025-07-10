978fdaa75c35061c3050fd32e102ac24ef2e1db9
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-07-10 11:39:25 +0300

    added: Not Found page with 10s redirect to home or immediate return

--- src/app/features/not-found/pages/not-found-page/not-found-page.component.ts
+++ src/app/features/not-found/pages/not-found-page/not-found-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
@@ -10,9 +11,25 @@ import {Component, OnInit} from '@angular/core';
 export class NotFoundPageComponent implements OnInit{
 
   protected message!: string;
+  protected countdown = 10;
+  private intervalId!: number;
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    this.message = 'message from .ts'
+    this.message = 'Oops! The page you’re looking for doesn’t exist or has been moved.';
+
+    this.intervalId = window.setInterval(() => {
+      this.countdown--;
+      if (this.countdown === 0) {
+        clearInterval(this.intervalId);
+        this.router.navigate(['home']);
+      }
+    }, 1000);
   }
 
+  redirectNow(): void {
+    clearInterval(this.intervalId);
+    this.router.navigate(['home']);
+  }
 }
