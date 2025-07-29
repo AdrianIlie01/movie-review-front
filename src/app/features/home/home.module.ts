65f4a262fdefe8984c4a0bc8ac43f4bd97b3ac62
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-07-29 20:09:33 +0300

    import new components for search, filter, carousels and modules for icons

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -4,17 +4,34 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeaderComponent } from './components/home-header/home-header.component';
 import {SharedModule} from '../../shared/shared.module';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MovieCarouselComponent } from './components/movie-carousel/movie-carousel.component';
+import { SearchFilterComponent } from './components/search-filter/search-filter.component';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { CastAndMovieCarouselComponent } from './components/cast-and-movie-carousel/cast-and-movie-carousel.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    HomeHeaderComponent
+    HomeHeaderComponent,
+    MovieCarouselComponent,
+    SearchFilterComponent,
+    CastAndMovieCarouselComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
 })
-export class HomeModule { }
+export class HomeModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far, fab);
+  }
+}
