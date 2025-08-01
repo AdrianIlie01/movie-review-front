098c8d82df0e2cedc770831d5a557b2da13c1584
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-08-01 17:59:09 +0300

    Add constants for skeleton loaders matching pagination data load counts

---
+++ src/app/shared/utils/constants.ts
@@ -0,0 +1,4 @@
+import {environment} from '../../../environments/environment';
+
+export const TOP_CAST_MOVIE_COUNT = Array(10).fill(null);
+export const LIMIT_LOADING_DATA = Array(environment.limit).fill(null);
