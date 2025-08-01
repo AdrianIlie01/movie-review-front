b637e590fd3c120eab9247a2195e930c597ed04c
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-08-01 17:37:39 +0300

    Add global route loader in AppComponent to show loading indicator during navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,41 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-refresh';
+  showLoader = false;
+  navigationFailed = false;
+  private loaderTimeout: any;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: Event) => {
+      // navigate to a route
+      if (event instanceof NavigationStart) {
+        this.showLoader = true;
+        this.navigationFailed = false;
+
+        // fallback for failed navigation
+        this.loaderTimeout = setTimeout(() => {
+          this.navigationFailed = true;
+        }, 10000);
+      }
+
+      // navigation ended or failed
+      if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.clearLoader();
+      }
+    });
+  }
+
+  clearLoader() {
+    this.showLoader = false;
+    this.navigationFailed = false;
+    clearTimeout(this.loaderTimeout);
+  }
 }
